3d252044e38932ff05b28201a620838e9b42db19
vuhoangthaiduong <vuhoangthaiduong1196@gmail.com>
2020-11-25 21:05:17 +0700

    Not done yet

---
+++ question-2.rb
@@ -0,0 +1,7 @@
+class Cat
+    def meow
+        puts "I'm a cat, gimme food & pet me now!"
+    end
+end
+
+Cat.new().meow()
---
+++ question-3.rb
@@ -0,0 +1,59 @@
+class Animal
+    attr_accessor :name
+
+    def initialize name
+        self.name = name
+    end
+    
+    def say_hello
+        puts "Hello!"
+    end
+end
+
+class Dog < Animal
+    def say_hello
+        puts "\nGau Gau, I'm a Dog. My name's #{self.name}"
+    end
+end
+
+class Cat < Animal
+    def say_hello
+        puts "\nMeow Meow, I'm a Cat. My name's #{self.name}"
+    end
+end
+
+class Zoo
+    def initialize
+        @@all_animals = Array.new
+    end
+    
+    def add_animal animal
+        @@all_animals << animal
+    end
+
+    def add_animals animals
+        @@all_animals.concat(animals)
+    end
+
+    def remove_animal index
+        @@all_animals.delete_at(index)
+    end
+
+    def remove_all_animals
+        @@all_animals = []
+    end
+
+    def show_all_animals
+        for i in 0..(@@all_animals.length - 1)
+            puts @@all_animals[i].say_hello
+        end
+    end
+end
+
+tom_cat = Cat.new("Tom")
+goofy_dog = Dog.new("Goofy")
+zoo = Zoo.new()
+zoo.add_animal(tom_cat)
+zoo.add_animal(goofy_dog)
+zoo.add_animals([Cat.new("Tabby"), Dog.new("Shiba")])
+zoo.show_all_animals()
